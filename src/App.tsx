c50fa7c8e82249caa1367c763caf263118f41783
Rafael <rafaelpereiradearaujo5@gmail.com>
2026-01-23 10:47:19 -0300

    Adiciona estrutura inicial do projeto com componentes Container, Heading e Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+
+import './styles/theme.css';
+import './styles/global.css';
+
 export function App() {
   console.log('Oi');
 
   return (
     <>
-      <h1>Olá mundo!</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem
-        corrupti nobis excepturi hic dolore, eum itaque ut praesentium enim
-        saepe quo doloremque nisi incidunt a consequatur blanditiis, magni atque
-        eos.
-      </p>
+      <Container>
+        <Logo />
+      </Container>
     </>
   );
 }
